ef620502ab05fc289b1c2f5ffd879299f720d039
Алексей Бельченков <u608110@gmail.com>
2022-01-16 13:31:20 +0400

    Landing - Structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,9 +5,35 @@ import mainLogo from '../assets/images/main.svg';
 
 const Landing = () => {
     return (
-        <div>
-
-        </div>
+        <main>
+            <nav>
+                <img
+                    src={logo}
+                    className='logo'
+                    alt='jobify'
+                />
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>job <span>tracking</span> app</h1>
+                    <p>
+                        Cray tattooed next level chartreuse, bitters echo park pug ugh woke.
+                        Flexitarian fashion axe poke activated charcoal locavore. Asymmetrical occupy master cleanse,
+                        kitsch deep v distillery live-edge. Leggings wolf flannel pug meditation lumbersexual.
+                        Plaid yuccie sustainable live-edge intelligentsia vaporware shaman slow-carb etsy tumeric
+                        tilde air plant.
+                    </p>
+                    <button className="btn btn-hero">
+                        Login/Register
+                    </button>
+                </div>
+                <img
+                    src={mainLogo}
+                    className='img main-img'
+                    alt="Main Logo"
+                />
+            </div>
+        </main>
     );
 };
 
