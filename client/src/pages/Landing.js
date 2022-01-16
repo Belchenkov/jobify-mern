447d0707dbf5bda70564b03998ceb342bf75e2be
Алексей Бельченков <u608110@gmail.com>
2022-01-16 13:24:42 +0400

    Landing - Basic Setup

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logo from '../assets/images/logo.svg';
+import mainLogo from '../assets/images/main.svg';
+
+const Landing = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Landing;
