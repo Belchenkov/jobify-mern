1e5a9fa55da0ab556da2056d2ad9605e5040da43
Алексей Бельченков <u608110@gmail.com>
2022-01-23 14:38:03 +0400

    Install React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+
 import Landing from './pages/Landing';
 
 function App() {
   return (
-      <div>
-          <Landing />
-      </div>
+      <BrowserRouter>
+          <Routes>
+              <Route
+                  path='/'
+                  element={
+                      <div>Dashboard</div>
+                  }
+              />
+              <Route
+                  path='/register'
+                  element={
+                      <div>Register</div>
+                  }
+              />
+              <Route
+                  path='/landing'
+                  element={<Landing />}
+              />
+              <Route
+                  path='*'
+                  element={
+                      <h1>Error</h1>
+                  }
+              />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
