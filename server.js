2454182f9eeac21b3fba5de21a67a7aa6649ebb2
Алексей Бельченков <u608110@gmail.com>
2022-02-20 15:24:34 +0400

    Connect to DB

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import connectDB from './db/connect.js';
+
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import connect from './db/connect.js';
 
 dotenv.config();
 
@@ -18,6 +21,16 @@ app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3005;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on http://127.0.0.1:${PORT}`);
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening on http://127.0.0.1:${PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+start();
