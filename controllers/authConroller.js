f150fb69b689868bb5602c0be771b8777d6e060f
Алексей Бельченков <u608110@gmail.com>
2022-05-01 16:09:57 +0400

    Check For Empty Values in Controller

--- controllers/authConroller.js
+++ controllers/authConroller.js
@@ -3,7 +3,17 @@ import { StatusCodes } from "http-status-codes";
 import User from '../models/User.js';
 
 const register = async (req, res) => {
-    const user = await User.create(req.body);
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        throw Error('Please provide all values!');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
 
     res.status(StatusCodes.CREATED)
         .json({
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -5,7 +5,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: 'Something went wrong, try again later'
+        msg: err.message || 'Something went wrong, try again later'
     };
 
     if (err.name === 'ValidationError') {
