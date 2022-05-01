870c6e6954a4b6078e9064a693d3b09a65914fbe
Алексей Бельченков <u608110@gmail.com>
2022-05-01 15:55:24 +0400

    Unique Field Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,6 +13,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         defaultError.msg = Object.values(err.errors).map(item => item.message).join(',');
     }
 
+    if (err.code & err.code === 11000) {
+        defaultError.statusCode = StatusCodes.UNPROCESSABLE_ENTITY;
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+    }
+
     return res
         .status(defaultError.statusCode)
         .json({
