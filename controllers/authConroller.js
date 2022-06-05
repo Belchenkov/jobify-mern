e24b62883d82b690fc6458a3c797c0585eef1a32
Алексей Бельченков <u608110@gmail.com>
2022-06-05 16:31:42 +0400

    Login Controller - login method

--- controllers/authConroller.js
+++ controllers/authConroller.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
 import User from '../models/User.js';
-import { UnprocessableEntityError } from "../errors/index.js";
+import { UnAuthenticatedError, UnprocessableEntityError } from '../errors/index.js';
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -31,7 +31,35 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    res.send('login user');
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new UnprocessableEntityError('Please provide all values');
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        throw new UnAuthenticatedError('Invalid Credentials');
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+        throw new UnAuthenticatedError('Invalid Credentials');
+    }
+
+    res.status(StatusCodes.OK)
+        .json({
+            status: true,
+            user: {
+                email: user.email,
+                lastName: user.lastName,
+                location: user.location,
+                name: user.name,
+                token: user.createJWT(),
+            },
+        });
 };
 
 const updateUser = async (req, res) => {
