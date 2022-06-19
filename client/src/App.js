473b4971e26bc14569ca938d444cd7daf31a6957
Алексей Бельченков <u608110@gmail.com>
2022-06-19 16:54:04 +0400

    Dashboard routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,22 @@ import {
 import Landing from './pages/Landing';
 import Register from './pages/Register';
 import Error from './pages/Error';
-import Dashboard from './pages/Dashboard';
+import AllJobs from './pages/dashboard/AllJobs'
+import Profile from './pages/dashboard/Profile';
+import SharedLayout from './pages/dashboard/SharedLayout';
+import Stats from './pages/dashboard/Stats';
+import AddJob from './pages/dashboard/AddJob';
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route
-                  path='/'
-                  element={<Dashboard />}
-              />
+              <Route path='/'>
+                  <Route path='stats' element={<Stats />} />
+                  <Route path='all-jobs' element={<AllJobs />} />
+                  <Route path='add-job' element={<AddJob />} />
+                  <Route path='profile' element={<Profile />} />
+              </Route>
               <Route
                   path='/register'
                   element={<Register />}
---
+++ client/src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SharedLayout = () => {
+    return (
+        <div>Shared</div>
+    );
+};
+
+export default SharedLayout;
