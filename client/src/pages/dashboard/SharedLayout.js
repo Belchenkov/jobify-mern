e82a66c740b49207c68291ac54bb9ba4dabea199
Алексей Бельченков <u608110@gmail.com>
2022-06-26 17:10:31 +0400

    Make Structure

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import Wrapper from '../../assets/wrappers/SharedLayout';
+import Navbar from "../../components/Navbar";
+import BigSidebar from "../../components/BigSidebar";
+import SmallSidebar from "../../components/SmallSidebar";
 
 const SharedLayout = () => {
     return (
         <Wrapper>
-            <nav>
-                <Link to="add-job">add job</Link>
-                <Link to="all-jobs">all jobs</Link>
-            </nav>
-            <Outlet />
+            <main className="dashboard">
+                <SmallSidebar />
+                <BigSidebar />
+                <div>
+                    <Navbar />
+                    <div className="dashboard-page">
+                        <Outlet />
+                    </div>
+                </div>
+            </main>
         </Wrapper>
     );
 };
