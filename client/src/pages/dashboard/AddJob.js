bd1a08dba29dd8dbffabafb90743e5b4f6ce1d9f
Алексей Бельченков <u608110@gmail.com>
2022-09-25 11:07:17 +0400

    Job page structure

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,8 +1,79 @@
 import React from 'react';
 
+import FormRow from '../../components/FormRow';
+import Alert from '../../components/Alert';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const AddJob = () => {
+    const {
+        isEditing,
+        showAlert,
+        displayAlert,
+        position,
+        company,
+        jobLocation,
+        jobType,
+        jobTypeOptions,
+        statusTypeOptions,
+        status,
+    } = useAppContext();
+
+    const handleJobInput = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        console.log(name, 'name');
+        console.log(value, 'value');
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (!position || !company || !jobLocation) {
+            displayAlert();
+            return;
+        }
+
+        console.log('create job');
+    };
+
     return (
-        <h1>Add Job</h1>
+        <Wrapper>
+            <form className="form">
+                <h3>{ isEditing ? 'edit job' : 'add job' }</h3>
+                { showAlert && <Alert /> }
+                <div className="form-center">
+                    <FormRow
+                        type='text'
+                        name="position"
+                        value={position}
+                        handleChange={handleJobInput}
+                    />
+                    <FormRow
+                        type='text'
+                        name="company"
+                        value={company}
+                        handleChange={handleJobInput}
+                    />
+                    <FormRow
+                        type='text'
+                        name="jobLocation"
+                        labelText="Job Location"
+                        value={jobLocation}
+                        handleChange={handleJobInput}
+                    />
+
+                    <div className="btn-container">
+                        <button
+                            type='submit'
+                            className='btn btn-block submit-btn'
+                            onClick={handleSubmit}
+                        >submit</button>
+                    </div>
+                </div>
+            </form>
+        </Wrapper>
     );
 };
 
