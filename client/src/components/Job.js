38ff9a1d2cb1e1490880068d1aadcc4f33221feb
Алексей Бельченков <u608110@gmail.com>
2022-10-23 16:07:16 +0400

    Apply moment.js

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,7 +1,12 @@
-const Job = ({ company }) => {
+import moment from 'moment';
+
+const Job = ({ company, createdAt }) => {
+    const date = moment(createdAt).format('MMM Do, YYYY');
+
     return (
         <>
             <h5>{company}</h5>
+            <h5>{date}</h5>
         </>
     );
 };
