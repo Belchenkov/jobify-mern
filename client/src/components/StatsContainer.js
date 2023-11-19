24a2a37e8fc3aa4bd2e0e00adbf8bde44e3e9d71
Алексей Бельченков <u608110@gmail.com>
2023-11-19 15:35:10 +0400

    Stats Container

---
+++ client/src/components/StatsContainer.js
@@ -0,0 +1,12 @@
+import StatsItem from './StatsItem';
+
+const StatsContainer = () => {
+    return (
+        <div>
+            <h1>Stats container</h1>
+            <StatsItem />
+        </div>
+    );
+};
+
+export default StatsContainer;
--- client/src/pages/dashboard/Stats.js
+++ client/src/pages/dashboard/Stats.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+import { useAppContext } from '../../context/appContext';
+import StatsContainer from '../../components/StatsContainer';
+import Loading from '../../components/Loading';
+import ChartsContainer from '../../components/ChartsContainer';
 
 const Stats = () => {
+    const { showStats, isLoading, monthApplications } = useAppContext();
+
+    useEffect(() => {
+        showStats();
+    }, []);
+
+    if (isLoading) {
+        return <Loading center />
+    }
+
     return (
-        <h1>Stats</h1>
+        <>
+            <StatsContainer />
+
+            { monthApplications?.length > 0 && <ChartsContainer /> }
+        </>
     );
 };
 
