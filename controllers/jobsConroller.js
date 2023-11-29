2bd83fbec605a1e702087f5aef565b681a928390
Алексей Бельченков <u608110@gmail.com>
2023-11-29 14:18:51 +0400

    refactor data

--- controllers/jobsConroller.js
+++ controllers/jobsConroller.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 import Job from '../models/Job.js';
 import UnprocessableEntityError from '../errors/unprocessable-entity.js';
@@ -98,15 +99,13 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-    let stats  = await Job.aggregate([
+    let stats = await Job.aggregate([
         { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
-        { $group: { _id: '$status', count: { $sum: 1 } } }
+        { $group: { _id: '$status', count: { $sum: 1 } } },
     ]);
-
     stats = stats.reduce((acc, curr) => {
         const { _id: title, count } = curr;
         acc[title] = count;
-
         return acc;
     }, {});
 
@@ -120,19 +119,28 @@ const showStats = async (req, res) => {
         { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
         {
             $group: {
-                _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt'} },
+                _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
                 count: { $sum: 1 },
-            }
+            },
         },
         { $sort: { '_id.year': -1, '_id.month': -1 } },
         { $limit: 6 },
     ]);
+    monthlyApplications = monthlyApplications
+        .map((item) => {
+            const {
+                _id: { year, month },
+                count,
+            } = item;
+            const date = moment()
+                .month(month - 1)
+                .year(year)
+                .format('MMM Y');
+            return { date, count };
+        })
+        .reverse();
 
-    res.status(StatusCodes.OK)
-        .json({
-            defaultStats,
-            monthlyApplications,
-        });
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export {
